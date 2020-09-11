d14607ebfa1f336f027ee72389bde97cba7711f6
Flargy <marlun1996@hotmail.com>
2020-09-11 17:28:17 +0200

    changes to raycasts

---
+++ AngularPerceptionTrick/Assets/Scripts/Direction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Direction
+{
+
+    public Vector3 vec;
+    public float mag;
+
+    public Direction(Vector3 vector, float magnitude)
+    {
+        vec = vector;
+        mag = magnitude;
+    }
+}
--- AngularPerceptionTrick/Assets/Scripts/PlayerMovement.cs
+++ AngularPerceptionTrick/Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,16 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movementVector = Vector3.zero;
     private Vector3 cameraRotation = Vector3.zero;
-    private float turnInput = 0.0f;
+
     private RaycastHit hit;
     private GameObject heldObject = null;
     private bool holdingObject = false;
 
     private float originalDistanceToPlayer = 0f;
     private Vector3 originalScale = Vector3.zero;
+    private Collider heldObjectCollider = null;
+    private Vector3 extents = Vector3.zero;
+    private List<Direction> boundCorners = new List<Direction>();
    
     void Update()
     {
@@ -25,6 +28,15 @@ public class PlayerMovement : MonoBehaviour
         if(holdingObject == true)
         {
             PushObject();
+        }
+
+        if(boundCorners.Count > 0)
+        {
+            foreach(Direction dir in boundCorners)
+            {
+                
+                Debug.DrawRay(transform.position, transform.TransformDirection(dir.vec) * dir.mag, Color.red);
+            }
         }
     }
 
@@ -49,12 +61,50 @@ public class PlayerMovement : MonoBehaviour
         {
             if(Physics.SphereCast(transform.position, 1f, transform.forward, out hit, Mathf.Infinity, mask))
             {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
                 heldObject = hit.collider.gameObject;
                 heldObject.GetComponent<Rigidbody>().useGravity = false;
                 heldObject.GetComponent<Rigidbody>().isKinematic = true;
                 originalDistanceToPlayer = Vector3.Distance(transform.position, heldObject.transform.position);
                 originalScale = heldObject.transform.localScale;
+                heldObjectCollider = heldObject.GetComponent<Collider>();
+                extents = heldObjectCollider.bounds.extents;
                 holdingObject = true;
+
+                Vector3 center = heldObjectCollider.bounds.center;
+
+                Vector3 corner;
+
+                corner = center + extents;
+                //boundCorners.Add(new Direction((corner - transform.position).normalized, Vector3.Distance(transform.position, corner)));
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center - extents;
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(-extents.x, -extents.y, extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(-extents.x, extents.y, extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(-extents.x, extents.y, -extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(extents.x, -extents.y, -extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(extents.x, -extents.y, extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+                corner = center + new Vector3(extents.x, extents.y, -extents.z);
+                boundCorners.Add(new Direction(transform.InverseTransformPoint(corner).normalized, Vector3.Distance(transform.position, corner)));
+
+
+
+
+
 
             }
         }
@@ -64,26 +114,36 @@ public class PlayerMovement : MonoBehaviour
             heldObject.GetComponent<Rigidbody>().isKinematic = false;
             heldObject = null;
             holdingObject = false;
+            heldObjectCollider = null;
         }
     }
 
+    //private void PushObject()
+    //{
+
+    //    if (Physics.Raycast(transform.position ,transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("Default")))
+    //    {
+    //        ScaleObject();
+    //        Vector3 distanceFromHit = (transform.position - heldObject.transform.position).normalized* ((heldObject.GetComponent<SphereCollider>().radius + 0.1f )* heldObject.transform.localScale.x);
+    //        heldObject.transform.position = hit.point + distanceFromHit;
+    //    }
+
+    //}
+
+    //private void ScaleObject()
+    //{
+    //    float scale = Vector3.Distance(heldObject.transform.position, transform.position) / originalDistanceToPlayer * originalScale.x;
+    //    Vector3 newScale = new Vector3(scale, scale, scale);
+
+    //    heldObject.transform.localScale = newScale;
+    //}
     private void PushObject()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("Default")))
-        {
-            Vector3 distanceFromHit = hit.normal * heldObject.GetComponent<SphereCollider>().radius * heldObject.transform.localScale.x;
-            heldObject.transform.position = hit.point + distanceFromHit;
-            ScaleObject();
-        }
-
+        
     }
 
     private void ScaleObject()
     {
-        float scale = Vector3.Distance(heldObject.transform.position, transform.position) / originalDistanceToPlayer * originalScale.x;
-        Vector3 newScale = new Vector3(scale, scale, scale);
 
-        heldObject.transform.localScale = newScale;
     }
-
 }
